5a65b95728d4872b8f300752150799add521b200
Tony Jiang <tonyjiang9398@gmail.com>
2014-12-14 21:36:05 -0500

    make both links open in new tab

--- views/home.php
+++ views/home.php
@@ -4,7 +4,7 @@
       $id = vget('_id');
       $company = vget('company');
       echo vlinkto('<input type="button" value="View Profile" />', 'recruiter', array('id' => $id), true);
-      echo vlinkto('<input type="button" value="View Company Profile" />', 'company', array('id' => $company));
+      echo vlinkto('<input type="button" value="View Company Profile" />', 'company', array('id' => $company), true);
     ?>
   </div>
-</panel>
+
