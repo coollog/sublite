dfb7067dc82cb9f4bd0742909e903fe6d31bb05c
Qingyang Chen <qingyang.chen@gmail.com>
2014-12-16 10:51:37 -0500

    changed config to sublite domain

--- config.php
+++ config.php
@@ -7,6 +7,6 @@
   $GLOBALS = array_merge($GLOBALS, array(
     'dbname' => $dbname,
     'dburi' => "mongodb://$dbuser:$dbpass@ds051980.mongolab.com:51980/$dbname",
-    'domain' => "54.172.31.140"
+    'domain' => "sublite.net/employers"
   ));
 ?>
