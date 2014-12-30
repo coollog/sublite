e186a49c1703f2241a9bea919d0f99c599635a0c
Tony Jiang <tonyjiang9398@gmail.com>
2014-12-30 12:04:03 -0800

    add legal checkbox to registration

--- views/register.php
+++ views/register.php
@@ -9,6 +9,9 @@
       <div class="form-slider"><label for="email">Email</label><input type="email" id="email" name="email" value="<?php vecho('email'); ?>" required /></div>
       <div class="form-slider"><label for="pass">Password (6 chars min)</label><input type="password" id="pass" name="pass" required pattern=".{6,}" /></div>
       <div class="form-slider"><label for="pass2">Confirm Password</label><input type="password" id="pass2" name="pass2" required pattern=".{6,}" /></div>
+      <left><input type="checkbox" name="terms" id="terms" value="agree" required /> <label for="terms">I represent and warrant that I am employed by, or am an authorized agent of, the company on behalf of which I will create a company profile along with any job or internship postings. All information I provide will be accurate and not misleading. I understand that SubLite does not guarantee the effectiveness of finding a suitable candidate or make representations as to the amount of applications we will receive.</label></left>
+      <br>
+      <left><input type="checkbox" name="terms2" id="terms2" value="agree" required /> <label for="terms2">I have read, understand, and agree to be bound by the <a href="terms.php" style="color:#035d75">Terms of Service.</a></label></left>
       <?php vnotice(); ?>
       <input type="submit" name="register" value="Register" />
     </form>
