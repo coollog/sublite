5dfd2ad25f6185b746b3920ff9c9841dad7beaab
Qingyang Chen <qingyang.chen@gmail.com>
2014-12-30 14:03:08 -0500

    fixed config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
   $GLOBALS = array_merge($GLOBALS, array(
     'dbname' => $dbname,
     'dburi' => "mongodb://$dbuser:$dbpass@ds051980.mongolab.com:51980/$dbname",
-    'dbnamestudent' => ,
+    'dbnamestudent' => $dbnamestudent,
     'dburistudent' => "mongodb://$dbuserstudent:$dbpassstudent@ds047057.mongolab.com:47057/$dbnamestudent",
     'domain' => "sublite.net/employers",
     'gmailpass' => $gmailpass,
