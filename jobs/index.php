b80473b80ab3da515ae0882a91087f340cd0eee5
Tony Jiang <tonyjiang9398@gmail.com>
2015-01-03 23:49:03 -0800

    fix dirpre

--- jobs/index.php
+++ jobs/index.php
@@ -1,5 +1,5 @@
 <?php
-  $GLOBALS['dirpre'] = '../employers/devq/';
+  $GLOBALS['dirpre'] = '../';
   require_once($GLOBALS['dirpre'].'includes/header.php');
   $CStudent->index();
   require_once($GLOBALS['dirpre'].'includes/footer.php');
