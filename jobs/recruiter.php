32d4d7324cd8b7e56a85ac240809309373c62d1b
Qingyang Chen <qingyang.chen@gmail.com>
2015-01-12 22:35:10 -0500

    fixed message header

---
+++ jobs/recruiter.php
@@ -0,0 +1,6 @@
+<?php
+  $GLOBALS['dirpre'] = '../employers/';
+  require_once($GLOBALS['dirpre'].'includes/header.php');
+  $CRecruiter->view();
+  require_once($GLOBALS['dirpre'].'includes/footer.php');
+?>
