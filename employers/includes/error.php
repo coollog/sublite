6e4a9f577fc6c3e5eeb98512174fc99d96717d54
Qingyang Chen <qingyang.chen@gmail.com>
2015-01-28 19:32:24 -0500

    changed header order so that no timezone error

--- employers/includes/error.php
+++ employers/includes/error.php
@@ -1,5 +1,5 @@
 <?php
-  error_reporting(E_ALL);
+  error_reporting(E_ALL & ~E_STRICT);
   ini_set('display_errors', '1');
   ini_set("log_errors", 1);
   ini_set("error_log", $GLOBALS['dirpre']."../errors.log");
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $GLOBALS['dirpre'] = 'employers/';
   require_once($GLOBALS['dirpre'].'includes/header.php');
-  //$CStudent->index();
-  // require_once($GLOBALS['dirpre'].'includes/footer.php');
+  $CStudent->index();
+  require_once($GLOBALS['dirpre'].'includes/footer.php');
 ?>
