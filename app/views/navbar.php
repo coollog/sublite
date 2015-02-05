d3e235ebc3fee6bb2587a45b397c96ece735aa55
Tony Jiang <tonyjiang9398@gmail.com>
2015-02-04 21:40:59 -0500

    fix critical navbar error

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -59,16 +59,25 @@
   <a href="."><logo>SubLite</logo></a><beta>beta</beta>
   <options class="right">
     <?php if (vget('Loggedin')) { ?>
-
-      <?php if (vget('Lcompany')) { ?>
-        <a href="addjob.php"><opt>List Job</opt></a>
-        <a href="home.php"><opt>Manage</opt></a>
-        <a href="messages.php"><opt>Messages</opt></a>
+      <?php if ($_SERVER['REQUEST_URI'] == '/') { ?>
+        <?php if (vget('Lcompany')) { ?>
+          <a href="employers/addjob.php"><opt>List Job</opt></a>
+          <a href="employers/home.php"><opt>Manage</opt></a>
+          <a href="employers/messages.php"><opt>Messages</opt></a>
+        <?php } else { ?>
+          <a href="employers/addcompany.php"><opt>Add Company Profile</opt></a>
+        <?php } ?>
+        <a href="logout.php"><opt>Log Out</opt></a>
       <?php } else { ?>
-        <a href="addcompany.php"><opt>Add Company Profile</opt></a>
+        <?php if (vget('Lcompany')) { ?>
+          <a href="addjob.php"><opt>List Job</opt></a>
+          <a href="home.php"><opt>Manage</opt></a>
+          <a href="messages.php"><opt>Messages</opt></a>
+        <?php } else { ?>
+          <a href="addcompany.php"><opt>Add Company Profile</opt></a>
+        <?php } ?>
+        <a href="logout.php"><opt>Log Out</opt></a>
       <?php } ?>
-      <a href="logout.php"><opt>Log Out</opt></a>
-
     <?php } elseif (vget('Loggedinstudent')) { ?>
 
       <a href="<?php echo $GLOBALS['dirpre']; ?>../housing"><opt>Search For Housing</opt></a>
