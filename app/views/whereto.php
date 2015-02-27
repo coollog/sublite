8c8dea64788d1db5ef6a0782380c3df9a9b309e1
Qingyang Chen <qingyang.chen@gmail.com>
2015-02-26 19:24:22 -0500

    fixed whereto

--- app/views/whereto.php
+++ app/views/whereto.php
@@ -2,7 +2,7 @@
   <div class="content">
     <headline>Looking for...?</headline>
 
-    <a href="<?php echo $GLOBALS['dirpre']; ?>../housing"><input type="button" value="Housing" /></a>
+    <a href="<?php echo $GLOBALS['dirpre']; ?>../housing/search.php"><input type="button" value="Housing" /></a>
     <a href="<?php echo $GLOBALS['dirpre']; ?>../jobs/search.php"><input type="button" value="Internships" /></a>
   </div>
 </panel>
