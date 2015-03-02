dc1837c8dc08823072f8cb8c8b7fe5614d5c6adb
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-02 00:16:38 -0500

    changed facebook like system

--- app/includes/htmlheader.php
+++ app/includes/htmlheader.php
@@ -115,7 +115,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=642157445814007";
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=478408982286879&version=v2.0";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
     
