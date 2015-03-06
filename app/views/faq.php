55627e2aa1ea4c894a59e0292888df1d6d78d1c6
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-05 19:39:06 -0500

    fixed faq3

--- app/views/faq.php
+++ app/views/faq.php
@@ -41,7 +41,8 @@
 
       <subheadline>Can I share an internship listing with a friend not registered on SubLite?</subheadline>
 
-
+      <br />
+      <br />
       <headline class="small">Housing</headline>
 
       <subheadline>Who can create SubLite accounts?</subheadline>
