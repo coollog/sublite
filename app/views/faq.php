7071abfeff8b79b48a736e48bc59725108676e45
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-05 19:36:27 -0500

    fixed faq

--- app/views/faq.php
+++ app/views/faq.php
@@ -5,6 +5,14 @@
   a:hover {
     color: #ffd800;
   }
+  headline.small {
+    font-size: 2em;
+    letter-spacing: 1px;
+    line-height: 1.5em;
+    margin: 0;
+    display: block;
+    text-align: center;
+  }
 </style>
 
 <panel class="faq">
@@ -12,6 +20,7 @@
     <headline>Frequently Asked Questions</headline>
 
     <left>
+      <headline class="small">Jobs</headline>
       <subheadline>Who can create an internship listing?</subheadline>
 
       Only verified HR personnel and recruiters can register for a company profile and post internships. Once they register for an account, we follow-up to make sure they are legitimate recruiters before activating their listing. 
@@ -20,7 +29,7 @@
 
       <subheadline>Who do I contact to find out more about an internship listing?</subheadline>
 
-      SubLite provides the platform for recruiters to create a company profile and post internships. A recruiter’s profile next to the listing will contain all the contact information you need to reach out and learn more. 
+      SubLite provides the platform for recruiters to create a company profile and post internships. A recruiter’s profile next to the listing will contain all the contact information you need to reach out and learn more.
 
       <subheadline>How do I apply for a job? </subheadline>
 
@@ -32,7 +41,44 @@
 
       <subheadline>Can I share an internship listing with a friend not registered on SubLite?</subheadline>
 
-      While you need to be a verified student with a .edu email to search housing and internship listings, you can share each with the listing’s url. Anyone who has the url will see the information although they won’t be able to interact with other registered users.
+
+      <headline class="small">Housing</headline>
+
+      <subheadline>Who can create SubLite accounts?</subheadline>
+      Any student at any accredited university.
+
+      <subheadline>What if I have a problem with my account?</subheadline>
+      Contact <a href="mailto: info@sublite.net">info@sublite.net</a> for any issues regarding usage of the website, including Log In and password issues.
+
+      <subheadline>How do I reserve a sublet?</subheadline>
+      Once you find a sublet that you like, contact the host and work out a deal!
+
+      <subheadline>How do I contact the host?
+      Every sublet listing page has a profile card beneath the photos with a button to Contact Owner.
+
+      <subheadline>If my space is no longer available, how do I take it down?</subheadline>
+      <subheadline>Can I hide my listing so I can reuse it next year?</subheadline>
+      Absolutely. We know that students may want to sublet the same space year after year. To make your life easier, we created a “publish-unpublish” function. You can unpublish your listing anytime by going to <a href="<?php vecho('dirpre'); ?>../housing/Manage.php">Manage</a>, clicking a listing to access its editing page, and unchecking the Publish? option. Next year when you’re ready to sublet again, simply check Publish? again to reactivate. You can also make edits to your listings anytime.
+
+      <subheadline>What legal issues should I consider before hosting on SubLite?</subheadline>
+      Make sure you have permission from your landlord or owner of your space to sublet. Before you list, you will have to agree that you can legally sublet according to our Terms of Use. 
+
+      Please also ensure all information in your listing is accurate and not misleading. For more guidance, refer to our Terms of Use.
+
+      <subheadline>When should I contact a host?</subheadline>
+      As soon as you find a space that interests you. Don’t wait until the last-minute, or it may be gone! 
+      <br />
+      Legitimate reasons to contact another user are to ask questions about the amenities and to inquire about price. Please contact hosts in good-faith. Spamming is expressly prohibited. For more guidance, refer to our <a href="<?php vecho('dirpre'); ?>../terms.php">Terms of Use</a>.
+
+      <subheadline>What will my listing and profile information be used for?</subheadline>
+      We will use this solely to enhance the search functionality of SubLite to make it easier to connect hosts and guests. We require your property’s address so other users can estimate proximity to their workplace. Your exact address will not be disclosed. 
+      <br />
+      We will not sell your information. You may see targeted advertising from sponsors based solely on non-identifying information such as your university and class year. We will not pass any of your information along to your universities, unless you violate the Terms of Use. 
+      <br />
+      For more information, read our <a href="<?php vecho('dirpre'); ?>../privacy.php">Privacy Policy</a>.
+
+      <subheadline>Where can I leave feedback?</subheadline>
+      Whether you like our website or not, we want to hear what you have to say. If you have a funny or horrific summer housing experience, we also want to hear from you. Email us at <a href="mailto: info@sublite.net">info@sublite.net</a>.
     </left>
   </div>
 </panel>
