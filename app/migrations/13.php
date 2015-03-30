e420d458bf7545983bb433d2df714c2d93d670aa
Qingyang Chen <qingyang.chen@gmail.com>
2015-03-29 20:21:28 -0400

    migration for getting rid of $ in front of prices

--- app/migrations/13.php
+++ app/migrations/13.php
@@ -4,7 +4,7 @@
   $sublets = $MSublet->getAll();
 
   foreach ($sublets as $sublet) {
-    if (preg_match('/^\$/'))
+    if (preg_match('/^\$/', $sublet['price']))
       echo $sublet['price'] = preg_replace('/^\$/', '', $sublet['price']).'<br/>';
 
     // $MSublet->save($sublet, false);
