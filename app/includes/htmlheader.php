2c058bd1c6a02e8dd4f7d32662cc86c9ab461dea
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-04 22:18:57 +0000

    random changes

--- app/includes/htmlheader.php
+++ app/includes/htmlheader.php
@@ -19,6 +19,7 @@
         foreach(vget('photos') as $photourl) {
           echo '<meta property="og:image" content="' . $photourl . '" />';
         }
+        echo '<title>' . $title . ' on SubLite</title>';
       }
       elseif (vget('firstname')) { // is recruiter profile
         $name = $viewVars['firstname'] . ' '. $viewVars['lastname'];
@@ -29,6 +30,7 @@
         echo '<meta property="og:image" content="' . $photo . '" />';
         echo '<meta property="og:description" content="Check out ' . $name .
           '\'s profile on SubLite!" />';
+        echo "<title>" . $name . "'s recruiter profile on SubLite</title>";
       }
       elseif (vget('deadline')) { // is job listing
         $name = vget('companyname');
@@ -44,6 +46,7 @@
         echo '<meta property="og:image:height" content="' . $height . '" />';
         echo '<meta property="og:description" content="Apply to the ' . $title .
           ' position on SubLite!" />';
+        echo '<title>' . $title . ' at ' . $name . ' &ndash; SubLite</title>';
       }
       elseif (vget('industry')) { // is company profile
         $name = vget('name');
@@ -58,6 +61,7 @@
         echo '<meta property="og:image:height" content="' . $height . '" />';
         $desc = vget('desc');
         echo '<meta property="og:description" content="' . $desc . '" />';
+        echo "<title>" . $name . " &ndash; Company profile on SubLite</title>";
       }
       elseif (strpos(htmlspecialchars($_SERVER['REQUEST_URI']), "/jobs") !== FALSE) { // on /jobs
     ?>
@@ -66,6 +70,7 @@
         <meta property="og:description" content="Attract the New Generation Talent with your Company's Unique Personality." />
         <meta property="og:image:width" content="1677" />
         <meta property="og:image:height" content="1118" />
+        <title>SubLite &ndash; Find internships and rent housing and sublets!</title>
     <?php
       }
       else { // default
@@ -77,6 +82,7 @@
         <meta property="og:description" content="Find summer internships and safe, student-only summer housing with SubLite! Verify your &quot;.edu&quot; email address to get started! It's completely free!" />
         <meta property="og:image:width" content="1000" />
         <meta property="og:image:height" content="1000" />
+        <title>SubLite &ndash; Find internships and rent housing and sublets!</title>
     <?php
       }
       $url = !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
@@ -86,7 +92,6 @@
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="SubLite" />
 
-    <title>SubLite &ndash; Your One-Stop Shop for a Great Summer!</title>
     <link rel="icon" type="image/png" href="<?php echo $GLOBALS['dirpre']; ?>assets/gfx/favicon.png" />
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
