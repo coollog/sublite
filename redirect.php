4c285d4a956d22583ff9adb522ffcd90695e77d5
Tony Jiang <tonyjiang9398@gmail.com>
2015-04-09 19:08:21 -0400

    functionality for mail

--- redirect.php
+++ redirect.php
@@ -2,6 +2,13 @@
   $GLOBALS['dirpre'] = 'app/';
   require_once($GLOBALS['dirpre'].'includes/header.php');
   $MJob->incrementApply($_GET['id']);
-  header("Location: " . $_GET['url']);
+  if(filter_var($_GET['url'], FILTER_VALIDATE_EMAIL)) {
+    header("Location: mailto:" . $_GET['url']);
+  }
+  else {
+    $link = $_GET['url'];
+    if (!preg_match('`^(https?:\/\/)`', $_GET['url'])) $link = "http://" . $link;
+    header("Location: " . $link);
+  }
   require_once($GLOBALS['dirpre'].'includes/footer.php');
 ?>
