a6601a8e92deedbc7f9263e70dd7f07bcefd0747
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-14 15:26:43 -0400

    added map graph

--- app/views/graph.php
+++ app/views/graph.php
@@ -11,52 +11,8 @@ Messages:<br />
 
 <script>
   var options = {
-
-    ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : true,
-
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
-
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,
-
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
-
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension : 0.4,
-
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
-
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 4,
-
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-
     //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
-
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
-
-    //String - A legend template
-    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
+    pointHitDetectionRadius : 5,
   };
 
   var data1 = {
@@ -164,3 +120,26 @@ Messages:<br />
   var ctx3 = $("#chart3").get(0).getContext("2d");
   var chart3 = new Chart(ctx3).Line(data3, options);
 </script>
+
+<?php if (isset($_GET['map'])) { ?>
+  <br />
+  Search Map:<br />
+  <canvas id="chart4" width="800" height="400"></canvas>
+  <script>
+    var data4 = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
+    var ctx4 = $("#chart4").get(0).getContext("2d");
+    var chart4 = new Chart(ctx4).Bar(data4, options);
+  </script>
+<?php } ?>
