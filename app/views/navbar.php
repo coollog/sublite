6d792adc3913d28b31af5f78cf694d63079e932d
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-21 22:32:33 -0400

    fixing list sublet link

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -60,8 +60,8 @@
   $states = array(
     "loggedin" => vget('Loggedin') or vget('Loggedinstudent'),
     "notloggedin" => !vget('Loggedin') and !vget('Loggedinstudent'),
-    "notloggedin !/employers" => !vget('Loggedin') and !vget('Loggedinstudent') and $curdir != '/employers',
-    "notloggedin /employers" => !vget('Loggedin') and !vget('Loggedinstudent') and $curdir == '/employers',
+    "notloggedin !/employers" => !vget('Loggedin') and !vget('Loggedinstudent') and !preg_match('/^\/employers/', $curdir),
+    "notloggedin /employers" => !vget('Loggedin') and !vget('Loggedinstudent') and preg_match('/^\/employers/', $curdir),
     "recruiter hascompany" => vget('Loggedin') and vget('Lcompany'),
     "recruiter nocompany" => vget('Loggedin') and !vget('Lcompany'),
     "student" => vget('Loggedinstudent'),
