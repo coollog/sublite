a5a47c96ac6efd6879efe9cf3306bc168b3b42cd
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-21 22:30:38 -0400

    fixing list sublet link

--- app/views/subletform.php
+++ app/views/subletform.php
@@ -67,6 +67,9 @@
       <br />
       <input type="checkbox" name="terms" id="terms" value="agree" required /> <label for="terms">I have read, fully understand, and agree to SubLite&rsquo;s <a href="terms.php">Terms of Service</a>. I represent and warrant that I have permission to list this property, and that the description is accurate and not misleading. I will negotiate the terms of the stay with potential guests in good-faith.</label>
 
+      <br />
+      Remember to share your listing on social media such as Facebook!
+
       <?php vnotice(); ?>
 
       <right>
