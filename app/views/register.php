aaf6ae3639a4f8638b67897f02c7ab8b6c0118e9
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-22 11:45:18 -0400

    fixed term link

--- app/views/register.php
+++ app/views/register.php
@@ -11,7 +11,7 @@
       <div class="form-slider"><label for="pass2">Confirm Password</label><input type="password" id="pass2" name="pass2" required pattern=".{6,}" /></div>
       <left><input type="checkbox" name="terms" id="terms" value="agree" required /> <label for="terms">I represent and warrant that I am employed by, or am an authorized agent of, the company on behalf of which I will create a company profile along with any job or internship postings. All information I provide will be accurate and not misleading. I understand that SubLite does not guarantee the effectiveness of finding a suitable candidate or make representations as to the amount of applications we will receive.</label></left>
       <br>
-      <left><input type="checkbox" name="terms2" id="terms2" value="agree" required /> <label for="terms2">I have read, understand, and agree to be bound by the <a href="terms.php" style="color:#035d75">Terms of Service.</a></label></left>
+      <left><input type="checkbox" name="terms2" id="terms2" value="agree" required /> <label for="terms2">I have read, understand, and agree to be bound by the <a href="<?php echo $GLOBALS['dirpre']; ?>../terms.php" style="color:#035d75">Terms of Service.</a></label></left>
       <?php vnotice(); ?>
       <input type="submit" name="register" value="Register" />
     </form>
