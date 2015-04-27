8cd050e6a03084549f768f8bb525f8689c7f46e3
Qingyang Chen <qingyang.chen@gmail.com>
2015-04-26 20:36:28 -0400

    fixed going back to index from hubs

--- hubs/start.php
+++ hubs/start.php
@@ -1,6 +1,6 @@
 <?php
   $GLOBALS['dirpre'] = '../app/';
   require_once($GLOBALS['dirpre'].'includes/header.php');
-  Controller::redirect('../index');
+  $CSocial->index();
   require_once($GLOBALS['dirpre'].'includes/footer.php');
 ?>
