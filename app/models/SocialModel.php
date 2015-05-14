0413f99ab22a5dba5031e1a0945ecb6b73bd3c54
Tony Jiang <tonyjiang9398@gmail.com>
2015-05-13 19:37:27 -0700

    start backend

--- app/models/SocialModel.php
+++ app/models/SocialModel.php
@@ -3,9 +3,27 @@
 
   class SocialModel extends Model {
     function __construct() {
-      $m = new MongoClient($GLOBALS['dburistudent']);
-      $db = $m->$GLOBALS['dbnamestudent'];
-      //$this->collection = $db->!!TODO!!;
+      parent::__construct('hubs');
+    }
+    function save($data) {
+      $this->collection->save($data);
+      return $data['_id']->{'$id'};
+    }
+    function get($id) {
+      return $this->collection->findOne(array('_id' => new MongoId($id)));
+    }
+    function isMember($hub, $student) {
+      $entry = $this->get($hub);
+      foreach ($entry['members'] as $key => & $sub_array) {
+        if($sub_array['id'] == $student) return true;
+      }
+      return false;
+    }
+    function add($hub, $student) {
+      $entry = $this->get($hub);
+      $entry['members'][] = array('time' => time(), 'id' => $student);
+      $this->save($entry, false);
+      return $entry['members'];
     }
   }
 
