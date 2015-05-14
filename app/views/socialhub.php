b43575dbe600d7b0f4b1908f3bda34749ff95a4b
Qingyang Chen <qingyang.chen@gmail.com>
2015-05-14 14:34:27 +0800

    added basic network interface code

--- app/views/socialhub.php
+++ app/views/socialhub.php
@@ -805,6 +805,37 @@
     });
   }
 
+  // Interface code for communication
+  var Comm = {
+    apiuri: 'api.php',
+    parse: function (content) {
+      var split = content.split('\n'),
+          name = split[0],
+          json = split[1];
+
+      json = JSON.parse(json);
+      var status = json.status,
+          data = json.data,
+          message = json.message;
+      return {
+        name: name,
+        status: status,
+        data: data,
+        message: message
+      };
+    },
+    emit: function (name, data, callback) {
+      var json = {
+        name: name,
+        data: data
+      };
+      $.post(apiuri, json, function (data) {
+        data = this.parse(data);
+        callback(data);
+      });
+    }
+  };
+
   // Actual code to set everything up for the first time
 
   Views.setup();
