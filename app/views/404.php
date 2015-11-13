6e94b25e137f8d4d5046cdb7805b60f335fc0b97
Qingyang Chen <qingyang.chen@gmail.com>
2015-11-12 20:47:42 -0500

    display route on 404

--- app/views/404.php
+++ app/views/404.php
@@ -2,5 +2,6 @@
   <div class="content">
     <headline>This page does not exist!</headline>
     <img height="300px" src="<?php echo $GLOBALS['dirpre'];?>assets/gfx/sadApe.png">
+    <route class="hide"><?php View::echof('uri'); ?></route>
   </div>
 </panel>
