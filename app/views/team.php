6bf3aefab83890898c0b2ff86e43c242c6747728
cyrieu <cyrieu@gmail.com>
2015-11-15 20:35:16 -0500

    fixed bug on team branch

--- app/views/team.php
+++ app/views/team.php
@@ -305,7 +305,8 @@
         box.style.backgroundSize = "160px 160px";
       });
 
-      $('img.memberimage').each(function (elem) {
+
+      $('#team img.memberimage').each(function (elem) {
         var src = $(this).attr('photo');
         $(this).attr('src', src);
       });
