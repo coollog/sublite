a6136c4504563b5ed92a52811508b29e1d9fe7be
Tony Jiang <tonyjiang9398@gmail.com>
2015-11-18 21:50:02 -0500

    fix weird space from jquery fix

--- app/includes/htmlheader.php
+++ app/includes/htmlheader.php
@@ -19,7 +19,7 @@
           href="data:<?php echo $GLOBALS['dirpre']; ?>assets/gfx/favicon.png" />
 
     <!-- JQUERY -->
-    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>-->
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
