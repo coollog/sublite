3101282ec80399568a54db7b45b2b56fbfa72c29
edwardshe <edwardshe1@yahoo.com>
2015-11-25 22:35:45 -0500

    Don't warn when profile is saved

--- app/views/faq.php
+++ app/views/faq.php
@@ -5,7 +5,23 @@
   a:hover {
     color: #ffd800;
   }
+  .msg_head {
+  cursor: pointer;
+  }
 </style>
+
+<script>
+$(document).ready(function()
+{
+  //hide the all of the element with class msg_body
+  $(".msg_body").hide();
+  //toggle the componenet with class msg_body
+  $(".msg_head").click(function()
+  {
+    $(this).next(".msg_body").slideToggle(600);
+  });
+});
+</script>
 
 <panel class="faq">
   <div class="content">
@@ -15,131 +31,131 @@
       <headline class="small">
         <a name="internships">Students - Internships</a>
       </headline>
-      <subheadline>What’s new on Internships 2.0?</subheadline>
+      <subheadline class="msg_head">What’s new on Internships 2.0?</subheadline>
 
-      Applying for internships and jobs has never been easier. You can now pre-write your answers standard application questions. Employers will choose from a pool of these questions in their applications, so applying can be done in a matter of seconds, not hours. Companies can view your profile, which has a general overview of what makes you unique, along with your qualifications and resume.
+      <div class="msg_body">Applying for internships and jobs has never been easier. You can now pre-write your answers standard application questions. Employers will choose from a pool of these questions in their applications, so applying can be done in a matter of seconds, not hours. Companies can view your profile, which has a general overview of what makes you unique, along with your qualifications and resume.</div>
 
-      <subheadline>How do I apply for a job?</subheadline>
+      <subheadline class="msg_head">How do I apply for a job?</subheadline>
 
-      On every listing there is a “Apply Now” button that will redirect you to the company’s application page. Your candidacy will only be considered if you submit an application through SubLite’s internal application system.
+      <div class="msg_body">On every listing there is a “Apply Now” button that will redirect you to the company’s application page. Your candidacy will only be considered if you submit an application through SubLite’s internal application system.</div>
 
-      <subheadline>Is there a limit on the number of internships I can apply to?</subheadline>
+      <subheadline class="msg_head">Is there a limit on the number of internships I can apply to?</subheadline>
 
-      Absolutely not! You may apply to as many internships as you wish.
+      <div class="msg_body">Absolutely not! You may apply to as many internships as you wish.</div>
 
-      <subheadline>Is there a word limit to my question responses?</subheadline>
+      <subheadline class="msg_head">Is there a word limit to my question responses?</subheadline>
 
-      No, you can answer your questions fully, with no word requirements to hold you back. You can fill in responses as needed for applications, or fill them all out at once. It’s up to you!
+      <div class="msg_body">No, you can answer your questions fully, with no word requirements to hold you back. You can fill in responses as needed for applications, or fill them all out at once. It’s up to you!</div>
 
-      <subheadline>Do I need to fill out my profile?</subheadline>
+      <subheadline class="msg_head">Do I need to fill out my profile?</subheadline>
 
-      Your profile is how recruiters get their first impression of you. If it’s not fully filled out, then what kind of first impression does that make?
+      <div class="msg_body">Your profile is how recruiters get their first impression of you. If it’s not fully filled out, then what kind of first impression does that make?</div>
 
-      <subheadline>Who do I contact to find out more about an internship listing?</subheadline>
+      <subheadline class="msg_head">Who do I contact to find out more about an internship listing?</subheadline>
 
-      SubLite provides the platform for recruiters to create a company profile and post internships. A recruiter’s profile next to the listing will contain all the contact information you need to reach out and learn more.
+      <div class="msg_body">SubLite provides the platform for recruiters to create a company profile and post internships. A recruiter’s profile next to the listing will contain all the contact information you need to reach out and learn more.</div>
 
-      <subheadline>Can I share an internship listing with a friend not registered on SubLite?</subheadline>
+      <subheadline class="msg_head">Can I share an internship listing with a friend not registered on SubLite?</subheadline>
 
-      While you need to be a verified student with a .edu email to search housing and internship listings, you can share each with the listing’s url. Anyone who has the url will see the information although they won’t be able to interact with other registered users.
+      <div class="msg_body">While you need to be a verified student with a .edu email to search housing and internship listings, you can share each with the listing’s url. Anyone who has the url will see the information although they won’t be able to interact with other registered users.</div>
 
       <br /> <br /> <br />
       <headline class="small">
         <a name="housing">Students - Housing</a>
       </headline>
 
-      <subheadline>Who can create SubLite accounts?</subheadline>
+      <subheadline class="msg_head">Who can create SubLite accounts?</subheadline>
 
-      Any student at any accredited university.
+      <div class="msg_body">Any student at any accredited university.</div>
 
-      <subheadline>What if I have a problem with my account?</subheadline>
+      <subheadline class="msg_head">What if I have a problem with my account?</subheadline>
 
-      Contact <a href="mailto: info@sublite.net">info@sublite.net</a> for any issues regarding usage of the website, including Log In and password issues.
+      <div class="msg_body">Contact <a href="mailto: info@sublite.net">info@sublite.net</a> for any issues regarding usage of the website, including Log In and password issues.</div>
 
-      <subheadline>How do I reserve a sublet?</subheadline>
+      <subheadline class="msg_head">How do I reserve a sublet?</subheadline>
 
-      Once you find a sublet that you like, contact the host and work out a deal!
+      <div class="msg_body">Once you find a sublet that you like, contact the host and work out a deal!</div>
 
-      <subheadline>How do I contact the host?</subheadline>
+      <subheadline class="msg_head">How do I contact the host?</subheadline>
 
-      Every sublet listing page has a profile card beneath the photos with a button to Contact Owner.
+      <div class="msg_body">Every sublet listing page has a profile card beneath the photos with a button to Contact Owner.</div>
 
-      <subheadline>
+      <subheadline class="msg_head">
         If my space is no longer available, how do I take it down?
         <br />
         Can I hide my listing so I can reuse it next year?
       </subheadline>
 
-      Absolutely. We know that students may want to sublet the same space year after year. To make your life easier, we created a “publish-unpublish” function. You can unpublish your listing anytime by going to <a href="<?php vecho('dirpre'); ?>../housing/Manage.php">Manage</a>, clicking a listing to access its editing page, and unchecking the Publish? option. Next year when you’re ready to sublet again, simply check Publish? again to reactivate. You can also make edits to your listings anytime.
+      <div class="msg_body">Absolutely. We know that students may want to sublet the same space year after year. To make your life easier, we created a “publish-unpublish” function. You can unpublish your listing anytime by going to <a href="<?php vecho('dirpre'); ?>../housing/Manage.php">Manage</a>, clicking a listing to access its editing page, and unchecking the Publish? option. Next year when you’re ready to sublet again, simply check Publish? again to reactivate. You can also make edits to your listings anytime.</div>
 
-      <subheadline>What legal issues should I consider before hosting on SubLite?</subheadline>
+      <subheadline class="msg_head">What legal issues should I consider before hosting on SubLite?</subheadline>
 
-      Make sure you have permission from your landlord or owner of your space to sublet. Before you list, you will have to agree that you can legally sublet according to our Terms of Use.
+      <div class="msg_body">Make sure you have permission from your landlord or owner of your space to sublet. Before you list, you will have to agree that you can legally sublet according to our Terms of Use.
 
-      Please also ensure all information in your listing is accurate and not misleading. For more guidance, refer to our Terms of Use.
+      Please also ensure all information in your listing is accurate and not misleading. For more guidance, refer to our Terms of Use.</div>
 
-      <subheadline>When should I contact a host?</subheadline>
+      <subheadline class="msg_head">When should I contact a host?</subheadline>
 
-      As soon as you find a space that interests you. Don’t wait until the last-minute, or it may be gone!
+      <div class="msg_body">As soon as you find a space that interests you. Don’t wait until the last-minute, or it may be gone!
       <br />
-      Legitimate reasons to contact another user are to ask questions about the amenities and to inquire about price. Please contact hosts in good-faith. Spamming is expressly prohibited. For more guidance, refer to our <a href="<?php vecho('dirpre'); ?>../terms.php">Terms of Use</a>.
+      Legitimate reasons to contact another user are to ask questions about the amenities and to inquire about price. Please contact hosts in good-faith. Spamming is expressly prohibited. For more guidance, refer to our <a href="<?php vecho('dirpre'); ?>../terms.php">Terms of Use</a>.</div>
 
-      <subheadline>What will my listing and profile information be used for?</subheadline>
+      <subheadline class="msg_head">What will my listing and profile information be used for?</subheadline>
 
-      We will use this solely to enhance the search functionality of SubLite to make it easier to connect hosts and guests. We require your property’s address so other users can estimate proximity to their workplace. Your exact address will not be disclosed.
+      <div class="msg_body">We will use this solely to enhance the search functionality of SubLite to make it easier to connect hosts and guests. We require your property’s address so other users can estimate proximity to their workplace. Your exact address will not be disclosed.
       <br />
       We will not sell your information. You may see targeted advertising from sponsors based solely on non-identifying information such as your university and class year. We will not pass any of your information along to your universities, unless you violate the Terms of Use.
       <br />
-      For more information, read our <a href="<?php vecho('dirpre'); ?>../privacy.php">Privacy Policy</a>.
+      For more information, read our <a href="<?php vecho('dirpre'); ?>../privacy.php">Privacy Policy</a>.</div>
 
-      <subheadline>Where can I leave feedback?</subheadline>
+      <subheadline class="msg_head">Where can I leave feedback?</subheadline>
 
-      Whether you like our website or not, we want to hear what you have to say. If you have a funny or horrific summer housing experience, we also want to hear from you. Email us at <a href="mailto: info@sublite.net">info@sublite.net</a>.
+      <div class="msg_body">Whether you like our website or not, we want to hear what you have to say. If you have a funny or horrific summer housing experience, we also want to hear from you. Email us at <a href="mailto: info@sublite.net">info@sublite.net</a>.</div>
 
       <br /> <br /> <br />
       <headline class="small">
         <a name="recruiters">Recruiters</a>
       </headline>
 
-      <subheadline>Who can create an internship listing?</subheadline>
+      <subheadline class="msg_head">Who can create an internship listing?</subheadline>
 
-      Only verified HR personnel and recruiters can register for a company profile and post internships. Once they register for an account, we follow-up to make sure they are legitimate recruiters before activating their listing.
+      <div class="msg_body">Only verified HR personnel and recruiters can register for a company profile and post internships. Once they register for an account, we follow-up to make sure they are legitimate recruiters before activating their listing.</div>
 
-      <subheadline>What’s new in the SubLite application system?</subheadline>
+      <subheadline class="msg_head">What’s new in the SubLite application system?</subheadline>
 
-      We’ve completely redesigned our search and application process. Students can now pre-write responses to common application questions and submit resumes, in order to save their time and yours.
+      <div class="msg_body">We’ve completely redesigned our search and application process. Students can now pre-write responses to common application questions and submit resumes, in order to save their time and yours.</div>
 
-      <subheadline>How do I make a listing?</subheadline>
+      <subheadline class="msg_head">How do I make a listing?</subheadline>
 
-      First, create a recruiter profile. After that, you may create a new listing. There you’ll be able to personalize your company’s profile, describe your listing, and choose which questions you’d like your applicants to answer. Click “post” and then you’re set!
+      <div class="msg_body">First, create a recruiter profile. After that, you may create a new listing. There you’ll be able to personalize your company’s profile, describe your listing, and choose which questions you’d like your applicants to answer. Click “post” and then you’re set!</div>
 
-      <subheadline>If I already have an internal application, can I link it?</subheadline>
+      <subheadline class="msg_head">If I already have an internal application, can I link it?</subheadline>
 
-      We know you may prefer to place a link to your company’s official website, but students may only apply through the SubLite website. Each listing profile is fully customizable; resume and student profile pages are automatically included in each application.
+      <div class="msg_body">We know you may prefer to place a link to your company’s official website, but students may only apply through the SubLite website. Each listing profile is fully customizable; resume and student profile pages are automatically included in each application.</div>
 
-      <subheadline>Is there a limit to the number of questions I can include in the application?</subheadline>
+      <subheadline class="msg_head">Is there a limit to the number of questions I can include in the application?</subheadline>
 
-      No, feel free to include any number of questions in your application!
+      <div class="msg_body">No, feel free to include any number of questions in your application!</div>
 
-      <subheadline>Is there a word limit to the answers of the questions?</subheadline>
+      <subheadline class="msg_head">Is there a word limit to the answers of the questions?</subheadline>
 
-      No, students are free to fully answer questions.
+      <div class="msg_body">No, students are free to fully answer questions.</div>
 
-      <subheadline>How much does it cost?</subheadline>
+      <subheadline class="msg_head">How much does it cost?</subheadline>
 
-      We charge nothing for you to list the job on our site, but to access the applicants it cost $8 per applicant for the first fifty, then $5 per applicant after that.
+      <div class="msg_body">We charge nothing for you to list the job on our site, but to access the applicants it cost $8 per applicant for the first fifty, then $5 per applicant after that.</div>
 
-      <subheadline>What happens if I don't use up all of my credits by the end of the recruiting season?</subheadline>
+      <subheadline class="msg_head">What happens if I don't use up all of my credits by the end of the recruiting season?</subheadline>
 
-      We have a full refund policy where we will refund you the full value of your remaining credits in your account. If you have used more than 50 credits, all unused credits are refunded at $5 each. If you have redeemed less than 50 credits, all unused credits are refunded at $8 each.
+      <div class="msg_body">We have a full refund policy where we will refund you the full value of your remaining credits in your account. If you have used more than 50 credits, all unused credits are refunded at $5 each. If you have redeemed less than 50 credits, all unused credits are refunded at $8 each.</div>
 
-      <subheadline>Will credits expire?</subheadline>
+      <subheadline class="msg_head">Will credits expire?</subheadline>
 
-      Absolutely not! You may choose to keep the credits in your account for usage in the following recruiting calendar. Credits never expire.
+      <div class="msg_body">Absolutely not! You may choose to keep the credits in your account for usage in the following recruiting calendar. Credits never expire.</div>
 
-      <subheadline>Can I get any credits for free?</subheadline>
+      <subheadline class="msg_head">Can I get any credits for free?</subheadline>
 
-      Your company will receive two bonus credits for creating a company profile and one bonus credit for each job listing you post.
+      <div class="msg_body">Your company will receive two bonus credits for creating a company profile and one bonus credit for each job listing you post.</div>
 
     </left>
   </div>
