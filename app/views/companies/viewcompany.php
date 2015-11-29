1624eb66038cb4fb0bebcd7c784bc6f1872124a7
Tony Jiang <tonyjiang9398@gmail.com>
2015-11-28 23:42:23 -0500

    optimize even more

--- app/views/companies/viewcompany.php
+++ app/views/companies/viewcompany.php
@@ -221,7 +221,7 @@
         // Don't want to move pictures or "The Company"
         if ((maxIndex == 0 && i == 0) || maxColumn.children().eq(i).is('img')) continue;
         var height = lengths[maxIndex][i];
-        if (Math.min(max - height, min + height) > bestMin + 50) {
+        if (Math.min(max - height, min + height) > bestMin) {
           bestMin = Math.min(max - height, min + height);
           bestMinIndex = i;
         }
