1ab6e3fbdf666e054501ee9d1a620d313caa0c1c
Qingyang Chen <qingyang.chen@gmail.com>
2015-12-01 18:19:42 -0500

    fixed quick opening route to same link

--- app/includes/functions/utilities.php
+++ app/includes/functions/utilities.php
@@ -201,4 +201,12 @@
       strpos($_SERVER["HTTP_USER_AGENT"], "facebookexternalhit/") !== false ||
       strpos($_SERVER["HTTP_USER_AGENT"], "Facebot") !== false;
   }
+
+  function currentPageUrl() {
+    $url  = @( $_SERVER["HTTPS"] != 'on' ) ?
+      'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+    $url .= ( $_SERVER["SERVER_PORT"] !== 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
+    $url .= $_SERVER["REQUEST_URI"];
+    return $url;
+  }
 ?>
