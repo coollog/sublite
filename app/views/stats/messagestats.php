efd5b00bdba69dfc5017ee6d046ff64b6e0819db
Qingyang Chen <qingyang.chen@gmail.com>
2015-12-10 21:32:27 -0500

    fixed messagestats background

--- app/views/stats/messagestats.php
+++ app/views/stats/messagestats.php
@@ -1,4 +1,7 @@
 <style>
+  .messagestats {
+    background: white;
+  }
   .thread {
     border: 1px solid #666;
     padding: 1em;
@@ -18,55 +21,57 @@
   }
 </style>
 
-<?php
-  $mlist = vget('mlist');
+<messagestats>
+  <?php
+    $mlist = vget('mlist');
 
-  foreach ($mlist as $m) {
-?>
-    <div class="thread">
-      <div class="brief">
+    foreach ($mlist as $m) {
+  ?>
+      <div class="thread">
+        <div class="brief">
+          <?php
+            $count = count($m['replies']);
+            $participants = array();
+            foreach ($m['participants'] as $p) {
+              $pname = $p['name'];
+              $pemail = $p['email'];
+              $participants[] = "$pname ($pemail)";
+            }
+            $participants = implode(", ", $participants);
+            $lasttime = $m['lasttime'];
+          ?>
+          <?php echo $count; ?> messages between <?php echo $participants; ?> | <?php echo $lasttime; ?>
+        </div>
         <?php
-          $count = count($m['replies']);
-          $participants = array();
-          foreach ($m['participants'] as $p) {
-            $pname = $p['name'];
-            $pemail = $p['email'];
-            $participants[] = "$pname ($pemail)";
-          }
-          $participants = implode(", ", $participants);
-          $lasttime = $m['lasttime'];
+          foreach ($m['replies'] as $r) {
+            $name = $r['name'];
+            $email = $r['email'];
+            $time = $r['time'];
+            $msg = $r['msg'];
+            $read = $r['read'] ? '' : 'unread';
+            $to = '';
+            foreach ($r['to'] as $t) {
+              $toname = $t['name'];
+              $toemail = $t['email'];
+              $to .= "$toname ($toemail) ";
+            }
         ?>
-        <?php echo $count; ?> messages between <?php echo $participants; ?> | <?php echo $lasttime; ?>
-      </div>
-      <?php
-        foreach ($m['replies'] as $r) {
-          $name = $r['name'];
-          $email = $r['email'];
-          $time = $r['time'];
-          $msg = $r['msg'];
-          $read = $r['read'] ? '' : 'unread';
-          $to = '';
-          foreach ($r['to'] as $t) {
-            $toname = $t['name'];
-            $toemail = $t['email'];
-            $to .= "$toname ($toemail) ";
+            <div class="reply <?php echo $read; ?>">
+              <div class="info">
+                <?php echo $name; ?> (<?php echo $email; ?>) to <?php echo $to; ?> | <?php echo $time; ?>
+              </div>
+              <div class="msg">
+                <?php echo $msg; ?>
+              </div>
+            </div>
+        <?php
           }
-      ?>
-          <div class="reply <?php echo $read; ?>">
-            <div class="info">
-              <?php echo $name; ?> (<?php echo $email; ?>) to <?php echo $to; ?> | <?php echo $time; ?>
-            </div>
-            <div class="msg">
-              <?php echo $msg; ?>
-            </div>
-          </div>
-      <?php
-        }
-      ?>
-    </div>
-<?php
-  }
-?>
+        ?>
+      </div>
+  <?php
+    }
+  ?>
+</messagestats>
 
 <script>
   $(function() {
