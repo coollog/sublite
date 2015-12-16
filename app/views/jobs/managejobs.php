5660e7c67ffa13ec2ce8da605a7f1f67eab8b6f6
Qingyang Chen <qingyang.chen@gmail.com>
2015-12-15 21:14:55 -0500

    json fix for managejobs

--- app/views/jobs/managejobs.php
+++ app/views/jobs/managejobs.php
@@ -63,12 +63,12 @@
   </nojobstemplate>
 </templates>
 
-<jobData class="hide">
+<textarea id="jobData" class="hide">
   <?php
     $jobs = View::get('jobs');
     echo toJSON($jobs);
   ?>
-</jobData>
+</textarea id="jobData">
 
 <script>
   function setupJobHover() {
@@ -81,7 +81,7 @@
 
   $(function () {
     (function setupJobs() {
-      var jobData = JSON.parse($('jobData').html());
+      var jobData = JSON.parse($('#jobData').html());
       jobData.forEach(function (job) {
         var _id = job._id.$id;
         var title = job.title;
