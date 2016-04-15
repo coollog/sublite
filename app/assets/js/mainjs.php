07ab604db7d2a55330a74855e0f4aa919599c323
Qingyang Chen <qingyang.chen@gmail.com>
2016-04-15 19:57:40 -0400

    temp

--- app/assets/js/mainjs.php
+++ app/assets/js/mainjs.php
@@ -60,6 +60,7 @@
     },
 
     use: function (templateName, data) {
+      console.log(templateName)
       var html = this.templateHTML[templateName];
       if (!isObject(data)) return this.error;
       for (var key in data) {
