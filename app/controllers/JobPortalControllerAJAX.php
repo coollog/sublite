87311be6a97b8b4af750e82219c95c9e0cac4697
Qingyang Chen <qingyang.chen@gmail.com>
2016-09-11 15:34:22 -0400

    Recent is now done in JobPortalControllerAJAX, probably

--- app/controllers/JobPortalControllerAJAX.php
+++ app/controllers/JobPortalControllerAJAX.php
@@ -24,7 +24,11 @@
     }
 
     public static function recent() {
+      global $params;
+      $skip = $params['skip'];
+      $count = $params['count'];
 
+      self::search([], $skip, $count);
     }
 
     public static function byIndustry() {
@@ -33,8 +37,34 @@
 
 
     // Takes a job document and extracts just the information to send back.
-    private static function normalizeJob() {
+    private static function normalizeJob(array $job) {
+      $job['_id'] = $job['_id']->{'$id'};
+      $company = CompanyModel::getById($job['company']);
+      $job['company'] = $company['name'];
+      $job['desc'] = strmax($job['desc'], 300);
+      $job['logophoto'] = $company['logophoto'];
+      return $job;
+    }
 
+    private static function processDBToView(array $data) {
+      $jobs = [];
+      foreach ($data as $job) {
+        $jobs[] = self::normalizeJob($job);
+      }
+      return $jobs;
+    }
+
+    private static function search(array $query, $skip, $count) {
+      $total = 0;
+      $res = JobModel::search($query, $skip, $count, $total);
+
+      $more = $skip + $count < $total;
+
+      // Save search to db.
+      if ($skip == 0) AppModel::recordSearch('jobs');
+
+      $jobs = self::processDBToView($res);
+      echo toJSON([ 'jobs' => $jobs, 'more' => $more, 'total' => $total ]);
     }
   }
 ?>
