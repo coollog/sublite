6ed2fe68e27229a1ebe790ecd5d5cecaac09efe3
edwardshe <edwardshe1@yahoo.com>
2016-09-18 21:10:43 -0400

    Popular Jobs done

--- app/controllers/JobPortalControllerAJAX.php
+++ app/controllers/JobPortalControllerAJAX.php
@@ -11,16 +11,26 @@
                                 implements JobPortalControllerAJAXInterface {
     public static function mostPopular() {
       global $params;
+      global $MJob;
       $skip = $params['skip'];
       $count = $params['count'];
 
-      // Build the query.
+      // Build the query; Issue the query.
+      $jobs = $MJob->getAll();
+      $topJobsSince = self::topViewersSince(90, $jobs, $skip, $count);
+      $multipleIds = array();
+      foreach($topJobsSince as $jobId) {
+        array_push($multipleIds, new MongoId($jobId));
+      }
 
-      // Issue the query.
-
-      // Massage the results.
+      $idQuery = array('$in' => $multipleIds);
+      $query = array('_id' => $idQuery);
+      $topJobs = $MJob->find($query);
 
       // Send back JSON.
+      $topJobsArray = iterator_to_array($topJobs);
+      $jobs = self::processDBToView($topJobsArray);
+      echo toJSON(['jobs' => $jobs]);
     }
 
     public static function recent() {
@@ -33,8 +43,46 @@
 
 
     // Takes a job document and extracts just the information to send back.
-    private static function normalizeJob() {
+    private static function normalizeJob(array $job) {
+      $job['_id'] = $job['_id']->{'$id'};
+      $company = CompanyModel::getById($job['company']);
+      $job['company'] = $company['name'];
+      $job['desc'] = strmax($job['desc'], 300);
+      $job['logophoto'] = $company['logophoto'];
+      return $job;
+    }
 
+    private static function processDBToView(array $data) {
+      $jobs = [];
+      foreach ($data as $job) {
+        $jobs[] = self::normalizeJob($job);
+      }
+      return $jobs;
+    }
+
+
+    private static function topViewersSince($days, $stats, $skip, $num) {
+      $time = time() - 86400*$days;
+
+      $statslength = sizeof($stats);
+      for($i = 0; $i < $statslength; $i++) {
+        $thisId = (string) $stats[$i]["_id"];
+        $viewers = isset($stats[$i]["viewers"]) ? $stats[$i]["viewers"] : array();
+        $count = 0;
+        for($j = 0; $j < sizeof($viewers); $j++) {
+          if($viewers[$j][1]->sec > $time) {
+            $count++;
+          }
+        }
+
+        $jobs[$thisId] = $count;
+      }
+
+      // Preferably, we could save this somewhere so we don't have to go through this every time.
+      arsort($jobs);
+      $jobs = array_slice($jobs, $skip, $num);
+      //return $jobs;
+      return array_keys($jobs);
     }
   }
 ?>
